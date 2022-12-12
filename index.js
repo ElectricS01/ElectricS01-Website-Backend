fa5e44a8a6f083ea5b7c008ea13fd92c8c0b035a
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2022-12-12 16:54:01 +1100

    1.0

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 24555
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
