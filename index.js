d77b60b821c889d7a8bd5c58ac5e9af69c904253
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2022-12-13 22:45:36 +1100

    1.1

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 24555
+const {Messages}=require('./models')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.get('/', async (req, res) => {
+    const messages = await Messages.findAll()
+    res.json(messages)
+})
+
+app.post('/', async (req, res) => {
+    const message = await Messages.create({
+        messageContents: req.body.messageContents,
+        userName: req.body.userName
+    })
+    res.json(message)
 })
 
 app.listen(port, () => {
---
+++ models/messages.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Messages extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Messages.init({
+    userName: DataTypes.STRING,
+    messageContents: DataTypes.STRING,
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Messages',
+  });
+  return Messages;
+};
