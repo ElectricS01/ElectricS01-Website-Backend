b08aa7074169b2f76004e146f182d5e78e161e6f
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-02-05 18:29:02 +1100

    1.25

--- models/sessions.js
+++ models/sessions.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Sessions.belongsTo(models.user, {
+      Sessions.belongsTo(models.Users, {
         as: "user"
       })
     }
