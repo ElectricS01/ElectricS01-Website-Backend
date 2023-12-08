3f1b47de140c91f920effa1388bc0cdf034a2e5b
ElectricS01 <103579308+ElectricS01@users.noreply.github.com>
2023-12-09 00:28:37 +1100

    1.92.11

--- migrations/20231127124436-userName-to-userId.js
+++ migrations/20231127124436-userName-to-userId.js
@@ -1,7 +1,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.renameColumn("messages", "username", "userId")
+    await queryInterface.renameColumn("Messages", "username", "userId")
     await queryInterface.changeColumn("Messages", "userId", {
       type: Sequelize.INTEGER
     })
